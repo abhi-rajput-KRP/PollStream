b7d914bc9571bcc78dab0296b8f038b6ad1c5ccc
Abhi Rajput <Kartarsinghabhi2007@gmail.com>
2026-02-06 10:47:08 +0530

    Setting up gunnicorn server

--- backend/server.py
+++ backend/server.py
@@ -150,4 +150,4 @@ def delete_polls():
         return jsonify({'message': 'Something went wrong!'}), 400
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app,debug=True , host='0.0.0.0', port=5000)
